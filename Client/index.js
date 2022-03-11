4202f97baa05c0f75b93eb6cfebac4c2e12cab02
nab138 <NicholasDanielSharp@gmail.com>
2022-03-10 20:46:05 -0500

    Fixed node_modules

--- Client/index.js
+++ Client/index.js
@@ -1,39 +1,18 @@
-const screenshot = require('screenshot-desktop')
-const sharp = require('sharp')
-const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 8080 })
-let interval;
-wss.on('connection', function connection(ws) {
-    interval = setInterval(async () => {
-        ws.send("hb")
-    }, 5000)
-    ws.on('message', async function incoming(message) {
-        console.log(message.toString())
-        if(message.toString().toLowerCase() == 'f'){
-            ws.send(await ss())
-        } else if(message.toString().toLowerCase() == 'i'){
-            clearInterval(interval)
-            ws.send(await ss())
-        }
-    })
-})
-async function ss(){
-    return new Promise((resolve, reject) => {
-    screenshot().then((img) => {
-        sharp(img)
-        .resize(384, 216)
-        .png()
-        .toBuffer()
-        .then(async (data) => {
-            resolve(data.toString('base64'))
-        })
-        .catch((err) => {
-            console.error(err)
-            reject(err)
-        });
-    }).catch((err) => {
-        console.error(err)
-        reject(err)
-    })
-})
-}
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 8080,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
